5456cf80ec2e2bc05ab0f899e026324cc537f755
Alvi Aziz <aaziz00@stuy.edu>
2018-12-10 14:03:09 -0500

    bubbleSort and main

--- Sorts.java
+++ Sorts.java
@@ -1,5 +1,41 @@
 public class Sorts{
-  public static void selectionsort(int[] ary){
+
+  public static void main(String[] args){
+    int[] data = new int[Integer.parseInt(args[0])];
+
+    for (int i = 0 ; i < data.length ; i++){
+      data[i] = (int) (Math.random() * 100);
+    }
+
+    System.out.println("Inital:");
+    String ans = "[";
+    for (int i = 0 ; i < data.length ; i++){
+      ans += data[i] + ", ";
+    }
+    ans = ans.substring(0,ans.length()-2) + "]";
+    System.out.println(ans);
+
+    if(args[1].equals("selectionSort")){
+      selectionSort(data);
+    }
+    if(args[1].equals("bubbleSort")){
+      bubbleSort(data);
+    }
+
+    System.out.println("Final:");
+    ans = "[";
+    for (int i = 0 ; i < data.length ; i++){
+      ans += data[i] + ", ";
+    }
+    ans = ans.substring(0,ans.length()-2) + "]";
+    System.out.println(ans);
+
+  }
+
+  //---------------------------------------------------------------------------
+
+
+  public static void selectionSort(int[] ary){
     //hold the the location of the smallest
     int whereSmall = 0;
 
@@ -19,4 +55,38 @@ public class Sorts{
     }
 
   }
+
+  //---------------------------------------------------------------------------
+
+  private static boolean bubbleSwap(int[] data, int index1, int index2){
+    //return true and swaps if the index1 is greater than index2 of data
+    if (data[index1] > data[index2]){
+      int placehold = data[index1];
+      data[index1] = data[index2];
+      data[index2] = placehold;
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean sorted(int[] data){
+    for (int i = 0 ; i < data.length - 1; i++){
+      if (data[i] > data[i+1]){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void bubbleSort(int[] data){
+    while(!sorted(data)){
+      for (int i = 0 ; i < data.length - 1 ; i++){
+        if (data[i] > data[i+1]){
+          bubbleSwap(data, i, i+1);
+        }
+      }
+    }
+
+  }
+
 }
--- mydriver.java
+++ mydriver.java
@@ -2,9 +2,11 @@ import java.util.List;
 import java.util.ArrayList;
 public class mydriver{
   public static void main(String[] args){
-    int[] data = {2,5,1,7,4,9,6,7};
-    //{1,2,4,5,6,7,7,9}
-    Sorts.selectionsort(data);
+    int[] data = new int[Integer.parseInt(args[0])];
+
+    for (int i = 0 ; i < data.length ; i++){
+      data[i] = (int) (Math.random() * 100);
+    }
 
     String ans = "[";
     for (int i = 0 ; i < data.length ; i++){
@@ -12,5 +14,16 @@ public class mydriver{
     }
     ans = ans.substring(0,ans.length()-2) + "]";
     System.out.println(ans);
+
+    //{1,2,4,5,6,7,7,9}
+    Sorts.bubbleSort(data);
+
+    ans = "[";
+    for (int i = 0 ; i < data.length ; i++){
+      ans += data[i] + ", ";
+    }
+    ans = ans.substring(0,ans.length()-2) + "]";
+    System.out.println(ans);
+
   }
 }
